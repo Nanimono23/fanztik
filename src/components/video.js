20b5a3cd869bc6dace0eb5bf8addd88ac055c0a2
Nanimono23 <zenigane007@gmail.com>
2024-05-27 21:57:57 +0900

    aa

--- src/components/video.js
+++ src/components/video.js
@@ -5,8 +5,7 @@ const VideoPost = ({ post, activePostId }) => {
   const videoRef = useRef(null);
 
   useEffect(() => {
-    console.log('Current active post ID:', activePostId);  // コンソールログを追加してアクティブなポストIDを確認
-    if (post._id === activePostId) {  // idを_idに修正
+    if (post.id === activePostId) {
       const playPromise = videoRef.current.play();
       if (playPromise !== undefined) {
         playPromise.catch((error) => {
@@ -17,12 +16,12 @@ const VideoPost = ({ post, activePostId }) => {
       videoRef.current.pause();
       videoRef.current.currentTime = 0;
     }
-  }, [activePostId, post._id]);  // idを_idに修正
+  }, [activePostId, post.id]);
 
   return (
     <div className="video-post">
-      <video ref={videoRef} src={post.url} controls loop playsInline /> {/* urlフィールドを使用 */}
-      <p>{post.description}</p> {/* descriptionフィールドを使用 */}
+      <video ref={videoRef} src={post.url} controls loop playsInline />
+      <p>{post.description}</p>
     </div>
   );
 };
